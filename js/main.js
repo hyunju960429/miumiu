22a807b171c6608ca6308e94aac3b6d8e21776f4
hyunju960429 <145514544+hyunju960429@users.noreply.github.com>
2024-01-25 11:56:45 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -51,6 +51,56 @@ locomotive();
 ///////////////
 
 
+// pre-road
+let container = document.querySelector('#progress');
+let progressBar = document.querySelector('.progress-bar');
+let progressText = document.querySelector('.progress-text');
+
+
+var imgLoad = imagesLoaded('body');
+let imgTotal = imgLoad.images.length;
+
+let imgLoaded = 0;
+let current = 0;
+let progressTimer;
+let topValue;
+
+
+progressTimer = setInterval(updateProgress, 1000/60);
+imgLoad.on('progress', function(){
+    imgLoaded++;
+
+});
+
+
+
+function updateProgress(){
+    let loadheart = document.querySelectorAll('.loadingWrap .miutext');
+    let target = (imgLoaded / imgTotal) * 100;
+    console.log(target)
+    current += (target - current) * 0.03;
+
+    progressBar.style.width = current + "%";
+    progressText.innerHTML = `${Math.ceil(current)}%`;
+
+
+    if(current > 99.9){
+        clearInterval(progressTimer)
+        container.classList.add('progress-complete');
+        progressBar.style.width = "100%";
+        gsap.to(container,{
+            duration: 0.3,
+            yPercent: -100,
+        })
+    }
+    gsap.to(loadheart,{y:-20, stagger: 0.5})
+}
+
+
+
+
+
+
 //smallMenu
 // let smMenu = document.querySelector('.smallMenu');
 // let closed = document.querySelector('.close');
